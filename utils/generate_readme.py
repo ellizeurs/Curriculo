01fdaaecb6407c6517ec845a54b556046ac6142c
Ellizeu Sena <ellizeurs@gmail.com>
2025-09-09 20:23:48 -0300

    Update readme template

--- utils/generate_readme.py
+++ utils/generate_readme.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 from urllib.parse import quote
 
-def generate_readme(template_path: str, output_path: str, pdf_folder: str):
+def generate_readme(dados: dict, template_path: str, output_path: str, pdf_folder: str):
     template_file = Path(template_path)
     env = Environment(
         loader=FileSystemLoader(template_file.parent),
@@ -15,7 +15,7 @@ def generate_readme(template_path: str, output_path: str, pdf_folder: str):
     files_list = "\n".join([f"- [{pdf.name}](https://github.com/ellizeurs/Curriculo/blob/main/PDFs/{quote(pdf.name)})" for pdf in pdfs])
 
     # Renderiza o template
-    content = template.render(FILES=files_list)
+    content = template.render(FILES=files_list, LINKEDIN=dados["personalInformation"].get("linkedin", ""), LATTES=dados["personalInformation"].get("lattes", ""))
 
     # Salva o README.md
     Path(output_path).write_text(content, encoding="utf-8")
