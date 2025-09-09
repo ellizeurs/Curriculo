ebbba08644d32b006391a333875441892aefc7dd
Ellizeu Sena <ellizeurs@gmail.com>
2025-09-09 16:38:08 -0300

    Implements parallelism

--- main.py
+++ main.py
@@ -7,40 +7,47 @@ from utils.generate_jinja_pdf import generate_jinja_pdf
 from utils.translate_json import translate_json
 from utils.generate_readme import generate_readme
 
-
-if __name__ == "__main__":
-    templates_path = Path("./templates")
-    with open("dados.json", "r", encoding="utf-8") as f:
-        dados = json.load(f)
-
+def generate_pdfs_for_translation(dados, translation, templates_path):
     name = dados["personalInformation"]["name"]
     first_name = name.split()[0]
     sur_name = name.split()[1:]
     dados["firstName"] = first_name
     dados["surName"] = " ".join(sur_name)
 
-    translations = dados.get("translations", {})
+    if translation["value"] == dados.get("language"):
+        dados_traduzidos = dados
+    else:
+        dados_traduzidos = asyncio.run(translate_json(dados, src=dados.get("language"), dest=translation["value"]))
+    with ProcessPoolExecutor() as executor:
+        futures = [
+            executor.submit(
+                generate_jinja_pdf,
+                templates_path / template / f"{template}.tex.template",
+                dados_traduzidos,
+                output_name=f"{first_name} - {template} - {translation['name']}",
+            )
+            for template in dados["templates"]
+        ]
+
+        for f in futures:
+            f.result()
+
+if __name__ == "__main__":
+    templates_path = Path("./templates")
+    with open("dados.json", "r", encoding="utf-8") as f:
+        dados = json.load(f)
 
     clean_output_directory(Path("./PDFs"))
 
-    for translation in translations:
-        if translation["value"] == dados.get("language"):
-            dados_traduzidos = dados
-        else:
-            dados_traduzidos = asyncio.run(translate_json(dados, src=dados.get("language"), dest=translation["value"]))
-        with ProcessPoolExecutor() as executor:
-            futures = [
-                executor.submit(
-                    generate_jinja_pdf,
-                    templates_path / template / f"{template}.tex.template",
-                    dados_traduzidos,
-                    output_name=f"{first_name} - {template} - {translation['name']}",
-                )
-                for template in dados["templates"]
-            ]
+    with ProcessPoolExecutor() as executor:
+        futures = [
+            executor.submit(generate_pdfs_for_translation, dados, translation, templates_path)
+            for translation in dados.get("translations", {})
+        ]
 
-            for f in futures:
-                f.result()
+        for f in futures:
+            f.result()
+
 
     # Gera o README.md
     generate_readme(
