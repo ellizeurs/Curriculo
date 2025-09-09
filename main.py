d7db45697052730e151c0648829ee9baa96a3822
Ellizeu Sena <ellizeurs@gmail.com>
2025-09-09 20:09:45 -0300

    Fix not a directory bugs

--- main.py
+++ main.py
@@ -8,13 +8,19 @@ from utils.generate_readme import generate_readme
 
 if __name__ == "__main__":
     templates_path = Path("./templates")
+    output_path = Path("./PDFs")
     with open("dados.json", "r", encoding="utf-8") as f:
         dados = json.load(f)
 
     print("Iniciando geração de PDFs...")
 
+    if output_path.exists() and not output_path.is_dir():
+        raise NotADirectoryError(f"O caminho {output_path} existe e não é um diretório.")
+    elif not output_path.exists():
+        output_path.mkdir(parents=True, exist_ok=True)
+
     # Limpa o diretório de saída
-    clean_output_directory(Path("./PDFs"))
+    clean_output_directory(output_path)
 
     # Gera os PDFs
     generate_all_pdfs(dados, templates_path)
@@ -23,7 +29,7 @@ if __name__ == "__main__":
     generate_readme(
         template_path="templates/README.md.template",
         output_path="README.md",
-        pdf_folder="PDFs"
+        pdf_folder=output_path
     )
 
     print("Arquivos gerados com sucesso.")
